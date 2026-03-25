29d993064f7cdd83a3032fe1c38249384b4e61ca
Murilo Enzo <muriloenzodesouza.meds@gmail.com>
2026-03-25 15:39:28 -0300

    feat: implementar criação dinâmica de cards para contatos

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
 'use strict';
 
+const contatos = [
+    { 
+        id: 1,
+        name: 'Ana Oliveira Dias',
+        celular: '+55 11 99999-9999',
+        foto: './img/ana-oliveira-dias.png',
+        email: 'ana.oliveira.dias@email.com',
+        endereco: 'Rua das Flores, 123, São Paulo - SP',
+        cidade: 'São Paulo',
+    },
+    {
+        id: 2,
+        name: 'Bruno Silva',
+        celular: '+55 11 98888-8888',
+        foto: './img/ana-oliveira-dias.png',
+        email: 'bruno.silva@email.com',
+        endereco: 'Avenida Paulista, 1000, São Paulo - SP',
+        cidade: 'São Paulo',
+    }
+];
+
 // função para criar o card
-function criarCard() {
+function criarCard(contatos) {
     const card = document.createElement('a'); 
 
     card.className = 'card';
     card.href = './pages/contato.html';
     card.innerHTML = `
-        <img src="./img/ana-oliveira-dias.png" alt="Imagem do contato" class="card__image">
-        <h2 class="card__name">Ana Oliveira Dias</h2>
+        <img src="${contatos.foto}" alt="Imagem do contato" class="card__image">
+        <h2 class="card__name">${contatos.name}</h2>
         <div class="card__contact-phone">
-            <i class=""fas fa-mobile-alt contact__icon"></i>
-            <p class="contact-phone__number">+55 11 99999-9999</p>
+            <i class="fas fa-mobile-alt contact__icon"></i>
+            <p class="contact-phone__number">${contatos.celular}</p>
         </div>
         <div class="card__tag-color"></div>
     `;
@@ -24,11 +45,9 @@ function carregarCards() {
     const container = document.getElementById('card-container');
 
    // inserindo na pagina HTML
-   for(let i = 0; i < 10; i++) {
-   container.appendChild(criarCard());
-    }
+   const cards = contatos.map(criarCard);
+   container.replaceChildren(...cards);
 
 
 }
-
 carregarCards();
